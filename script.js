69d3387fe99b67a81b6fd924f201f629710ddd30
Floryan Lollivier <floryan.lollivier@gmail.com>
2016-09-10 16:14:57 +0200

    ajout fichier js & changer popup image

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+            alert("Commentaire envoyé");
+        }
+
+function open_infos() {
+    window.open('elements/images/maquette1.png','Projet1','menubar=no, scrollbars=no, top=100, left=100, width=600, height=500');
+}
+
+//(http://www.supportduweb.com/scripts_tutoriaux-code-source-71-ouvrir-une-nouvelle-fenetre-pop-up-en-javascript-window-open-tutoriel.html)
+
+
+
