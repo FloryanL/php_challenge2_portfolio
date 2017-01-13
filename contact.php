27c3540a5a3ccbdbf31b7c41e1baa52f919369bf
FloryanL <floryan.lollivier@gmail.com>
2017-01-13 15:29:25 +0100

    add file contact.php

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+
+	if(!empty($errors)) {
+		header('Location: index.php');
+	}else{
+		$to = "floryan.lollivier@gmail.com";
+		$sujet = $_POST['prenom'] . ' ' . $_POST['nom'] . ' vous a contacté via le portfolio.';
+		$message = $_POST['message'];
+		$header = 'From : ' . $_POST['email'];
+		error_log("Envoi d'un email de ".$_POST['nom']." ".$_POST['prenom']." . ".$_POST['email']." Voici son msg:".$_POST['message']);
+		mail($to, $sujet, $message, $header);
+	}
+	
+?>
